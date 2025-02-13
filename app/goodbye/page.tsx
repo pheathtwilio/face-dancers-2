04a6788a347b63576a05f74d2472e2b069918ec9
pheathtwilio <pheath@twilio.com>
2025-02-13 14:57:37 -0600

    Altered goodbye page to look better and return the user to the waiting room

--- app/goodbye/page.tsx
+++ app/goodbye/page.tsx
@@ -1,5 +1,27 @@
-export default function Home() {
-    return (
-      <h1>Goodbye</h1>
-    )
+'use client'
+
+import React from 'react'
+import { Button, Container, Row, Col } from 'react-bootstrap'
+import { useRouter } from 'next/navigation'
+
+export default function GoodbyePage() {
+  
+  const router = useRouter()
+
+  const handleReturn = () => {
+    router.push('/') // Adjust the path as needed
   }
+
+  return (
+    <Container fluid className="vh-100 d-flex justify-content-center align-items-center">
+      <Row>
+        <Col className="text-center">
+          <h1>Goodbye</h1>
+          <Button variant="primary" onClick={handleReturn} className="mt-3">
+            Return to Waiting Room
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
