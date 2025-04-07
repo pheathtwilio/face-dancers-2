e41a3857ea1e2b4a9535989c8c2771794a15c37e
pheathtwilio <pheath@twilio.com>
2025-04-07 17:25:16 -0500

    adding ability to set session length in env variables

--- util/session-types.ts
+++ util/session-types.ts
@@ -1,5 +1,5 @@
 enum SessionTypes {
-    ITEM_TTL = 600
+    ITEM_TTL = parseInt(process.env.SESSION_LENGTH_IN_MILLISECONDS != null ? process.env.SESSION_LENGTH_IN_MILLISECONDS : '600')
 }
 
 export default SessionTypes
