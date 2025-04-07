c6f5796ad9c3e85e02f8988b75e5b449d825a7b0
pheathtwilio <pheath@twilio.com>
2025-04-07 16:06:43 -0500

    altered Sentry config to turn on off via values in the env variables.

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,16 +2,37 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs"
+
+const isEnabled = (process.env.DEBUG_ENABLED === 'true')
+const isDevelopment = (process.env.NODE_ENV === 'development')
+const dsnValue = `${process.env.SENTRY_DSN}`
+
+console.log(`DSN VALUE ${dsnValue}`)
+
+const beforeSendReturnType = async (
+  event: Sentry.ErrorEvent,
+  hint: Sentry.EventHint
+): Promise<Sentry.ErrorEvent | null> => {
+  if (isEnabled) {
+    return event
+  } else {
+    return null
+  }
+}
+
+const traceSampleRateConf = (isDevelopment? 1 : 0)
+const replaysSessionSampleRateConf = (isDevelopment ? 0.1 : 0)
+const replaysOnErrorSampleRateConf = (isDevelopment ? 1.0 : 0)
 
 Sentry.init({
 
-  enabled: (process.env.NODE_ENV === 'production'),
-  dsn: process.env.SENTRY_DSN,
+  enabled: isEnabled, 
+  dsn: process.env.SENTRY_DSN, 
 
   // return null to drop all events
-  beforeSend(e){
-    return null
+  beforeSend(event: Sentry.ErrorEvent, hint: Sentry.EventHint) {
+    return beforeSendReturnType(event, hint)
   },
 
   // Add optional integrations for additional features
@@ -20,16 +41,16 @@ Sentry.init({
   ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: (process.env.NODE_ENV === 'production'? 0 : 1),
+  tracesSampleRate: traceSampleRateConf, 
 
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: (process.env.NODE_ENV === 'production'? 0 : 0.1),
+  replaysSessionSampleRate: replaysSessionSampleRateConf, 
 
   // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: (process.env.NODE_ENV === 'production'? 0 : 1.0),
+  replaysOnErrorSampleRate: replaysOnErrorSampleRateConf, 
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: (process.env.DEBUG_ENABLED === 'true'),
+  debug: false
 });
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,21 +3,38 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs"
+
+const isEnabled = (process.env.DEBUG_ENABLED === 'true')
+const isDevelopment = (process.env.NODE_ENV === 'development')
+const dsnValue = `${process.env.SENTRY_DSN}`
+
+const beforeSendReturnType = async (
+  event: Sentry.ErrorEvent,
+  hint: Sentry.EventHint
+): Promise<Sentry.ErrorEvent | null> => {
+  if (isEnabled) {
+    return event
+  } else {
+    return null
+  }
+}
+
+const traceSampleRateConf = (isDevelopment? 1 : 0)
 
 Sentry.init({
 
-  enabled: (process.env.NODE_ENV === 'production'),
+  enabled: isEnabled, 
   dsn: process.env.SENTRY_DSN,
 
   // return null to drop all events
-  beforeSend(e){
-    return null
+  beforeSend(event: Sentry.ErrorEvent, hint: Sentry.EventHint) {
+    return beforeSendReturnType(event, hint)
   },
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: (process.env.NODE_ENV === 'production'? 0 : 1),
+  tracesSampleRate: traceSampleRateConf,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: (process.env.DEBUG_ENABLED === 'true'),
-});
+  debug: false, 
+})
